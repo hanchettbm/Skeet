e3999a532696617957355d4687a580ca892986ef
Baden <hanchettbm@gmail.com>
2020-06-15 17:39:08 -0600

    Initial Commit

---
+++ Skeet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Skeet
+//
+//  Created by Baden Hanchett on 6/15/20.
+//  Copyright © 2020 Baden Hanchett. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
